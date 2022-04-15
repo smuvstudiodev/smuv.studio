1692960b02f80bcc3565d71b4bafd2283574aa06
Nzaki Michael <ekisnzaki@gmail.com>
2022-04-15 18:02:40 +0100

    Fixed nav modal

--- components/nav-modal.jsx
+++ components/nav-modal.jsx
@@ -9,25 +9,18 @@ export default function NavModal({ options }) {
 
 
     return (
-        <div className='
-            absolute top-0 left-0 w-full
-            h-full min-h-screen bg-[rgba(0,0,0,0.85)] backdrop-blur-sm
-            flex justify-center items-center content-center
-            '>
-            <div className='text-2xl text-white bg-[#0A0A0D]  backdrop-blur-[20px] h-[510px] w-[520px] rounded-xl py-11 px-[71px]'>
+        <div className='nav-modal-container'>
+            <div className='nav-modal'>
                 <div className="relative">
-                    <div className="absolute -right-20 -top-24">
+                    <div className="absolute -right-8 -top-20 lg:-right-20 lg:-top-24">
                         <button onClick={() => closeModal()}>
-                            
-                            <Image src="/images/cancle-icon.png" alt="Cancel Icon" width={45} height={45} />
+                        <object type="image/svg+xml" className="w-8 h-8 lg:w- lg:h-12 " data="/images/cancle-icon.png">svg-animation</object>
                         </button>
                     </div>
                 </div>
 
                 <div>
-                <object type="image/svg+xml"  width={48} height={48} data="/images/Logo_Icon_Animation.svg">svg-animation</object>
-
-                {/* <Image src="/images/Logo_Icon_Animation.svg"  alt="Smuv Icon" width={48} height={48} /> */}
+                    <object type="image/svg+xml" className="w-6 h-6 lg:w-12 lg:h-12 " data="/images/Logo_Icon_Animation.svg">svg-animation</object>
                 </div>
                 <div className="flex modal-links">
                     <Link href="/">
@@ -40,9 +33,9 @@ export default function NavModal({ options }) {
                         <a className="modal-links-item">Blog</a>
                     </Link>
                 </div>
-                <div className="border-t-2 border-dashed border-[#252529] py-6">
-                    <span>Want a smuv experience?</span>
-                    <h4>Let{"'"}s talk at hi@smuvstudio.com</h4>
+                <div className=" border-t-2 border-dashed border-[#252529] py-6 lg:py-8">
+                    <span className="text-xs text-[#45454D] lg:text-base">Want a smuv experience?</span>
+                    <h4 className="text-[#919199] text-sm lg:text-xl">Let{"'"}s talk at <a href="mailto:hi@smuvstudio.com">hi@smuvstudio.com</a> </h4>
                 </div>
             </div>
         </div>
