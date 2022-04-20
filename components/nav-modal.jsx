e7680a500905a32f2d8495235c744e4e2f304fd7
Nzaki Michael <ekisnzaki@gmail.com>
2022-04-20 11:06:56 +0100

    Fixed Modal Issue

--- components/nav-modal.jsx
+++ components/nav-modal.jsx
@@ -17,9 +17,7 @@ export default function NavModal({ options }) {
     return (
         <div className='nav-modal-container backdrop-blur-[300px]' onClick={e=>closeByBody(e)}>
             <div className='nav-modal moving_card_once' onClick={e=>StopCloseByBody(e)}>
-                <div className="flex justify-between pt-6 pb-2 px-7 lg:py-12 lg:px-[60px] bg-[#101012] rounded-tl-xl
-
-rounded-tr-xl">
+                <div className="flex justify-between pt-6 pb-2 px-7 lg:py-5 lg:px-[40px] bg-[#101012] rounded-tl-xl rounded-tr-xl">
                     <object type="image/svg+xml" className="w-6 h-6 lg:w-12 lg:h-12 " data="/images/Logo_Icon_Animation.svg">svg-animation</object>
                     <div className="">
                         <button onClick={() => closeModal()}>
@@ -30,7 +28,7 @@ rounded-tr-xl">
                     </div>
                 
                 </div>
-                <div className="flex modal-links px-7 lg:px-[60px]">
+                <div className="flex modal-links px-7 lg:px-[45px]">
                     <Link href="/">
                         <a className="modal-links-item"  onClick={() => closeModal()}>Home</a>
                     </Link>
@@ -40,7 +38,7 @@ rounded-tr-xl">
                         <a href="https://medium.com/@smuvstudio" className="modal-links-item"  onClick={() => closeModal()}>Blog</a>
                    
                 </div>
-                <div className=" border-t-2 border-dashed border-[#252529] pt-5  px-7 lg:px-[60px]">
+                <div className=" border-t-2 border-dashed border-[#252529] pt-4  px-7 lg:px-[50px]">
                     <span className="text-xs text-[#45454D] lg:text-base">Want a smuv experience?</span>
                     <h4 className="text-[#919199] text-sm lg:text-xl">Let{"'"}s talk at <a href="mailto:start@smuvstudio.com">start@smuvstudio.com</a> </h4>
                 </div>
